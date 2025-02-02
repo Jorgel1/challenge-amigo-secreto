8690a004aeaeb64c4e698f55ce23de97d3b307d4
Jorgel1 <jorgegothicmetal@gamail.com>
2025-02-02 10:54:33 -0300

    criando as funcoes

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ let amigos = [];
 
 
 
+
 function adicionarAmigo() {
-    let amigo = document.querySelector('input').value;
-    if(amigo == '') {
+    let amigo = document.querySelector('input');
+    if(amigo.value == '') {
         alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(amigo.value);
+        amigo.value = '';
     }
 
+}
+
+function listarAmigos(listadeAmigos) {
+    //listaAmigos.innerHT
+
+    let listaOrdenada = document.getElementById('listaAmigos');
+
+    listadeAmigos.forEach(amigo => {
+        let li = document.createElement('li');
+        listaOrdenada.appendChild(li);
+    });
 }
