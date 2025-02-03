e26f423263d5d0468c7ac5311ce5ef0200ed9d6c
jorramos <95020694+Jorgel1@users.noreply.github.com>
2025-02-03 05:20:24 +0000

    Subindo apos concluir as funcoes

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
-
-
-
+let li = document.createElement('li');
+let listaOrdenada = document.getElementById('listaAmigos');
 
 
 function adicionarAmigo() {
     let amigo = document.querySelector('input');
-    if(amigo.value == '') {
+    if (amigo.value == '') {
         alert('Por favor, insira um nome.');
     } else {
         amigos.push(amigo.value);
         amigo.value = '';
+        mostrarLista(amigos);
     }
 
-    listarAmigos(amigos);
-}
+};
 
-function listarAmigos(listadeAmigos) {
-    //listaAmigos.innerHT
-
-    let listaOrdenada = document.getElementById('listaAmigos');
+function mostrarLista(listadeAmigos) {
+    li.innerHTML = '';
 
     listadeAmigos.forEach(amigo => {
-        let li = document.createElement('li');
+        let paragrafo = document.createElement('p');
+        paragrafo.innerHTML = amigo;
+        li.appendChild(paragrafo);
         listaOrdenada.appendChild(li);
     });
-}
+};
+
+function sortearAmigo() {
+    if (amigos.length > 0) {
+        li.innerHTML = '';
+        let amigoSorteado = Math.floor(Math.random() * amigos.length);
+
+        let paragrafo = document.createElement('p');
+        paragrafo.innerHTML = amigos[amigoSorteado];
+        li.appendChild(paragrafo);
+        listaOrdenada.appendChild(li);
+    }
+};
